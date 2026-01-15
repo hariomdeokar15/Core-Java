d7a6012fa76e24aaf0e1a8d3ecae845ad08d22d4
hariomdeokar15 <hariomdeokar15@gmail.com>
2026-01-15 09:51:52 +0530

    remaining classes added

--- CollectionFramework/src/com/Arraylist/ArrayListMethods.java
+++ CollectionFramework/src/com/Arraylist/ArrayListMethods.java
@@ -1,7 +1,9 @@
 package com.Arraylist;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ArrayListMethods {
@@ -45,6 +47,13 @@ public class ArrayListMethods {
 	    System.out.println(list.toString());
 	    
 	    System.out.println(list.toArray());
+	    
+	    List<Integer> list2 = Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10);
+	    
+	
+	    
+	    
+	    
 	
 	}
 	
---
+++ CollectionFramework/src/com/linkedlist/Methods.java
@@ -0,0 +1,50 @@
+package com.linkedlist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Methods {
+
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> list = new LinkedList<>();
+		
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
+		
+		System.out.println(list);//o/p = [10, 20, 30, 40, 50] due to toString method
+		
+		System.out.println(list.get(0));
+		
+		System.out.println(list.contains(40));
+		
+		list.add(1, 15);
+		System.out.println(list);
+		
+		list.remove(1);
+		System.out.println(list);
+		
+		list.remove(Integer.valueOf(30));
+		System.out.println(list);
+		
+		LinkedList<String> slist = new LinkedList<>(Arrays.asList("Apple", "Mango", "orange", "Papaiya"));
+		
+		slist.remove("Apple");
+		System.out.println(slist);
+		
+		slist.add("Peru");
+		System.out.println(slist);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
