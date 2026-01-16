73cd547ee02e842102c6dce64d24c99f904174c7
hariomdeokar15 <hariomdeokar15@gmail.com>
2026-01-16 15:35:22 +0530

    Example 3 changes added

--- CollectionFramework/src/com/comparator/example/Example3.java
+++ CollectionFramework/src/com/comparator/example/Example3.java
@@ -1,6 +1,7 @@
 package com.comparator.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 class Student{
 	
@@ -42,16 +43,20 @@ public class Example3 {
 		students.add(new Student("Atharv", 9.8, "912836E"));
 		students.add(new Student("Pritam", 7.8, "912836E"));
 		
-		students.sort((a, b)->{
-			if(a.getCgpa()-b.getCgpa()>0) {
-				return -1;
-			}else if(a.getCgpa()-b.getCgpa()<0) {
-				return 1;
-			}else {
-				return a.getName().compareTo(b.getName());
-			}
-			
-		});
+//		students.sort((a, b)->{
+//			if(a.getCgpa()-b.getCgpa()>0) {
+//				return -1;
+//			}else if(a.getCgpa()-b.getCgpa()<0) {
+//				return 1;
+//			}else {
+//				return a.getName().compareTo(b.getName());
+//			}
+//			
+//		});
+		
+		Comparator<Student> comparator = Comparator.comparing(Student::getCgpa).reversed().thenComparing(Student::getName);
+		
+		students.sort(comparator);
 		
 		for(Student s: students) {
 			
