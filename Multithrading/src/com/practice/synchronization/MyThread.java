81c6b46293e90df2e2a33ba9f1c92c28684f5e9b
hariomdeokar15 <hariomdeokar15@gmail.com>
2026-02-04 07:53:14 +0530

    Multithreading new example added

---
+++ Multithrading/src/com/practice/synchronization/MyThread.java
@@ -0,0 +1,24 @@
+package com.practice.synchronization;
+
+public class MyThread extends Thread{
+	
+	private Counter counter;
+	
+	public MyThread(Counter counter) {
+		this.counter = counter;
+	}
+	
+	public void run() {
+		for(int i = 0;  i<10; i++) {
+			System.out.println(counter.increament()); 
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
+}
